9fadaac6f09a0beee9a4f187e96e684875716b7e
Abdullah Sahin <48244771+sahin-abdullah@users.noreply.github.com>
2026-01-10 23:47:44 -0500

    Test Containers

--- src/test/java/com/rental/car/TestcontainersConfiguration.java
+++ src/test/java/com/rental/car/TestcontainersConfiguration.java
@@ -3,16 +3,31 @@ package com.rental.car;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-	@Bean
+	@Bean(initMethod = "start", destroyMethod = "stop")
 	@ServiceConnection
+	@SuppressWarnings("resource")
 	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+		DockerImageName image = DockerImageName.parse("postgis/postgis:16-3.4")
+				.asCompatibleSubstituteFor("postgres");
+		return new PostgreSQLContainer<>(image)
+				.withDatabaseName("rentaldb")
+				.withUsername("rental_user")
+				.withPassword("rental_pass");
+	}
+
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	@ServiceConnection(name = "redis")
+	@SuppressWarnings("resource")
+	GenericContainer<?> redisContainer() {
+		return new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
+				.withExposedPorts(6379);
 	}
 
 }
